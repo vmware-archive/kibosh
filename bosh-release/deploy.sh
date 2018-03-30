f38c43b1c921b750830cd7ffad001a1d16688de1
Matt Cholick <cholick@gmail.com>
2018-03-30 12:34:31 -0700

    Update pipeline and deploy scripts to reflect new packaging
    Co-authored-by: Jeenal Shah <jshah@pivotal.io>

--- bosh-release/deploy.sh
+++ bosh-release/deploy.sh
@@ -4,6 +4,19 @@ set -ex
 TMPDIR="tmp"
 mkdir -p $TMPDIR
 
+echo "Getting docker-bosh in place"
+
+docker_bosh_pkg_path=./${TMPDIR}/docker.tar.gz
+if [ -f "${docker_bosh_pkg_path}" ]; then
+    echo "Docker-bosh package already exist, skipping download"
+else
+    echo "Docker-bosh package doesn't exist, downloading"
+    url=https://github.com/cloudfoundry-incubator/docker-boshrelease/releases/download/v31.0.1/docker-31.0.1.tgz
+    wget ${url} -O "${docker_bosh_pkg_path}"
+fi
+
+bosh upload-release ${docker_bosh_pkg_path}
+
 echo "Adding blobs"
 
 bosh add-blob ./${TMPDIR}/kibosh.linux kibosh.linux
