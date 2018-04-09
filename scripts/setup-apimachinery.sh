8040210d7f82c0731a17eacee7b879ece5128293
Matt Cholick <cholick@gmail.com>
2018-04-09 07:43:28 -0700

    Remove source from version branch

--- scripts/setup-apimachinery.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-ls -lah ./vendor/k8s.io
-
-rm -rf ./vendor/k8s.io/{api,apimachinery,apiserver,client-go,metrics}
-cp -r ./vendor/k8s.io/kubernetes/staging/src/k8s.io/{api,apimachinery,apiserver,client-go,metrics} ./vendor/k8s.io
