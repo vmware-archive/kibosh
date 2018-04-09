bb857a73e4876dc5d6e50a46d1cf1668a4516fc8
Matt Cholick <cholick@gmail.com>
2018-04-09 10:47:31 -0700

    Working on ci pipeline
    Co-authored-by: Jeenal Shah <jshah@pivotal.io>

--- example-chart-bosh-release/deploy.sh
+++ example-chart-bosh-release/deploy.sh
@@ -7,9 +7,9 @@ set -e
 TMPDIR="tmp"
 mkdir -p $TMPDIR
 
-
-docker pull cfplatformeng/spacebears:latest
-docker save cfplatformeng/spacebears:latest -o ../example-chart/images/spacebears.latest.tgz
+# ci can't do this. Refactor script to optionally do it sometimes?
+# docker pull cfplatformeng/spacebears:latest
+# docker save cfplatformeng/spacebears:latest -o ../example-chart/images/spacebears.latest.tgz
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 tar -cvzf ./${TMPDIR}/helm_chart_src.tgz -C "${DIR}/../" example-chart
@@ -20,5 +20,3 @@ bosh add-blob ./${TMPDIR}/helm_chart_src.tgz helm_chart_src.tgz
 bosh create-release --name=example-chart --force
 
 bosh upload-release --name=example-chart
-
-
