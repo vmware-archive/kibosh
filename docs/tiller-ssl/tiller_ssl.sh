13122be6f4a1c79f72760d919064425762c9974d
Matt Cholick <cholick@gmail.com>
2018-08-02 08:24:10 -0700

    Fix up cert generation (& cleanup intermediate files)

--- docs/tiller-ssl/tiller_ssl.sh
+++ docs/tiller-ssl/tiller_ssl.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 set -ex
 
-openssl genrsa -out ./ca.key.pem 4096
+echo "Generate CA key and cert"
 
 cat > ca.conf <<EOF
-[req]
+[ req ]
 distinguished_name = req_distinguished_name
 req_extensions = v3_req
-[req_distinguished_name]
+[ req_distinguished_name ]
 countryName_default = US
 stateOrProvinceName_default = WA
 localityName_default = Seattle
 organizationalUnitName_default = Pivotal
+[ v3_ca ]
+subjectKeyIdentifier=hash
+authorityKeyIdentifier=keyid:always,issuer:always
+basicConstraints = CA:true
 [ v3_req ]
-basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 EOF
 
-openssl req -new -x509 -days 7300 -sha256 -out ca.csr -subj "/CN=*.kibosh.cf/O=Pivotal/C=US" -key ca.key.pem -config ca.conf
-#openssl req -key ca.key.pem -new -x509 -days 7300 -sha256 -out ca.cert.pem -extensions v3_ca
+openssl genrsa -out ./ca.key.pem 4096
+openssl req -key ca.key.pem -new -x509 -days 7300  -subj "/CN=*.kibosh.cf/O=Pivotal/C=US" -sha256 -out ca.cert.pem -extensions v3_ca -config ca.conf
 
+rm ca.conf
+
+
+echo "Generating tiller key and signed cert"
 
 cat > tiller.conf <<EOF
 [req]
@@ -36,10 +42,15 @@ keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 EOF
 
 openssl genrsa -out ./tiller.key.pem 4096
-openssl req -key tiller.key.pem -new -sha256  -subj "/CN=tiller..kibosh.cf/O=Pivotal/C=US" -out tiller.csr.pem -config ca.conf
+openssl req -key tiller.key.pem -new -sha256 -subj "/CN=tiller.kibosh.cf/O=Pivotal/C=US" -out tiller.csr.pem -config tiller.conf
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in tiller.csr.pem -out tiller.cert.pem -days 7300
 
+rm tiller.csr.pem
+rm tiller.conf
 
-cat > helm_cli.conf <<EOF
+echo "Generating helm key and signed cert"
+
+cat > helm.conf <<EOF
 [req]
 distinguished_name = req_distinguished_name
 req_extensions = v3_req
@@ -53,5 +64,11 @@ basicConstraints = CA:FALSE
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 EOF
 
-openssl genrsa -out ./helm_cli.key.pem 4096
-openssl req -key helm_cli.key.pem -new -sha256  -subj "/CN=helm.kibosh.cf/O=Pivotal/C=US" -out helm_cli.csr.pem -config ca.conf
+openssl genrsa -out ./helm.key.pem 4096
+openssl req -key helm.key.pem -new -sha256 -subj "/CN=helm.kibosh.cf/O=Pivotal/C=US" -out helm.csr.pem -config helm.conf
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in helm.csr.pem -out helm.cert.pem -days 7300
+
+rm helm.csr.pem
+rm helm.conf
+
+rm ca.srl
